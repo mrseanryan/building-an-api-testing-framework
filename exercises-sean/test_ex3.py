0fbf97d0f03e1de78330493c30c3992985a156f5
sean <sean.ryan@mendix.com>
2019-07-05 12:11:06 +0200

    Avoid dupe url in ex3

--- exercises-sean/test_ex3.py
+++ exercises-sean/test_ex3.py
@@ -2,6 +2,9 @@ import pytest
 import requests
 from pprint import pformat
 
+def build_url(suffix):
+    return 'http://localhost:8000/' + suffix
+
 def build_headers(token):
     headers = { 'user': 'sean', 'token': token }
     return headers
@@ -10,13 +13,13 @@ def build_headers(token):
 def create_book(get_token):
     book_json = {"title": "The Life and Times of Bob", "sub_title": None, "author": "Bob the Builder", "publisher": "Dorset House Publishing", "year": 2019, "pages": 666}
 
-    response = requests.post('http://localhost:8000/books', json = book_json, headers = build_headers(get_token))
+    response = requests.post(build_url('/books'), json = book_json, headers = build_headers(get_token))
     assert response.status_code == 201
     return response.json()
 
 @pytest.fixture()
 def get_token():
-    response = requests.post('http://localhost:8000/token/sean')
+    response = requests.post(build_url('token/sean'))
     assert response.status_code == 201
     token = response.json()['token']
     return token
@@ -26,7 +29,7 @@ def test_delete_book(get_token, create_book):
     book = create_book
     print("create_book: {}".format(pformat(book)))
 
-    url = 'http://localhost:8000/books/'+book['id']
+    url = build_url('books/'+book['id'])
     print("url={}".format(url))
     response = requests.delete(url, headers = build_headers(token))
 
