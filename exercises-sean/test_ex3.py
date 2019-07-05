abd4f23dd44a7c8c0da83e395c6e58714a1822e6
sean <sean.ryan@mendix.com>
2019-07-05 12:08:47 +0200

    Ex3 - use separate fixtures which is a bit cleaner

--- exercises-sean/test_ex3.py
+++ exercises-sean/test_ex3.py
@@ -7,10 +7,6 @@ def build_headers(token):
     return headers
 
 @pytest.fixture()
-def setup():
-    token = get_token()
-    return [token, create_book(token)]
-
 def create_book(get_token):
     book_json = {"title": "The Life and Times of Bob", "sub_title": None, "author": "Bob the Builder", "publisher": "Dorset House Publishing", "year": 2019, "pages": 666}
 
@@ -18,15 +14,16 @@ def create_book(get_token):
     assert response.status_code == 201
     return response.json()
 
+@pytest.fixture()
 def get_token():
     response = requests.post('http://localhost:8000/token/sean')
     assert response.status_code == 201
     token = response.json()['token']
     return token
 
-def test_delete_book(setup):
-    token = setup[0]
-    book = setup[1]
+def test_delete_book(get_token, create_book):
+    token = get_token
+    book = create_book
     print("create_book: {}".format(pformat(book)))
 
     url = 'http://localhost:8000/books/'+book['id']
